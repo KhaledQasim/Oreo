1731590779004af87fff993bd93d6e362cceea53
root <root@pop-os.localdomain>
2022-05-15 16:21:53 +0100

    added title to gallery

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -21,20 +21,16 @@
     <link rel="stylesheet" href="{{ asset('gallery-clean.css') }}">
 </head>
 <body>
+    <h1 style="text-align: center;">{{$photo->description}}</h1>
 <div class="container gallery-container">
     <div class="tz-gallery">
-        <div class="row">
-            <div>
-                <div>
-                    @forelse(json_decode($photo->images , true) as $image)
-                        <a class="lightbox " href="{{ Voyager::image($image) }}">
-                            <img class="img-fluid img-thumbnail" src="{{ Voyager::image($image) }}" alt="Missing Gallery Photo" style="padding:3px;margin:3px">
-                        </a>
-                    @empty
-                        <h4>No images selected in gallery! (go to admin webpage and add photos to the images column)</h4>
-                    @endforelse
-                </div>
-            </div>
+      @forelse(json_decode($photo->images , true) as $image)
+          <a class="lightbox " href="{{ Voyager::image($image) }}">
+              <img class="img-fluid img-thumbnail" src="{{ Voyager::image($image) }}" alt="Missing Gallery Photo" style="padding:3px;margin:3px">
+          </a>
+      @empty
+          <h4>No images selected in gallery! (go to admin webpage and add photos to the images column)</h4>
+      @endforelse
     </div>
 </div>
 
